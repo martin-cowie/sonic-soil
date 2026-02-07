bec0dc5b40c37cbc023651d6928bbd41a7d245eb
Martin Cowie <3288869+martin-cowie@users.noreply.github.com>
2026-02-07 22:36:09 +0000

    Upgrading to Sonor v2.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use futures::prelude::*;
 use sonor::Speaker;
-use std::{time::Duration, env::Args, collections::HashMap};
+use std::{time::Duration, env::Args, collections::BTreeMap};
 
 const MUSIC_SERVICE: &str = "urn:upnp-org:serviceId:MusicServices";
 
@@ -11,7 +11,10 @@ async fn main() -> Result<(), sonor::Error> {
     let argv0 = args.next().expect("Cannot get argv[0]");
 
     // Handle args
-    let subcommand = args.next().unwrap_or_else(|| panic!("wrong # args, try {argv0} subcommand"));
+    let subcommand = args.next().unwrap_or_else(|| {
+        eprintln!("Usage: {argv0} <list|join>");
+        std::process::exit(1);
+    });
 
     match subcommand.as_ref() {
         "list" => {
@@ -21,7 +24,8 @@ async fn main() -> Result<(), sonor::Error> {
             do_join(args).await?;
         }
         _ => {
-            panic!("Unknown subcommand, try {argv0} list|join")
+            eprintln!("Unknown subcommand '{}', try: {argv0} list|join", subcommand);
+            std::process::exit(1);
         }
     }
     
@@ -31,18 +35,25 @@ async fn main() -> Result<(), sonor::Error> {
 async fn do_join(mut args: Args) -> Result<(), sonor::Error> {
     
     if args.len() < 2 {
-        panic!("Supply at least two speaker zones to join");
+        eprintln!("Supply at least two speaker zones to join");
+        std::process::exit(1);
     }
 
     let speakers = get_speakers().await?;
     
     let group_master_name = args.next().unwrap();
-    let group_master_zone = speakers.get(&group_master_name).unwrap_or_else(|| panic!("Unknown zone: {}", group_master_name));
-    let group_master = group_master_zone.get(0).unwrap();
+    let group_master_zone = speakers.get(&group_master_name).unwrap_or_else(|| {
+        eprintln!("Unknown zone: {}", group_master_name);
+        std::process::exit(1);
+    });
+    let group_master = group_master_zone.first().unwrap();
 
     // join the master to all the others
     for arg in args {
-        let ref spkr: Speaker = speakers.get(&arg).unwrap_or_else(|| panic!("Unknown speaker {}", arg))[0];
+        let spkr = &speakers.get(&arg).unwrap_or_else(|| {
+            eprintln!("Unknown speaker: {}", arg);
+            std::process::exit(1);
+        })[0];
         join(group_master, spkr).await?;
 
         println!("Joined {arg} to {group_master_name}");
@@ -54,7 +65,6 @@ async fn do_join(mut args: Args) -> Result<(), sonor::Error> {
 async fn join(master: &Speaker, member: &Speaker) -> Result<(), sonor::Error> {
     let uri = format!("x-rincon:{}", master.uuid().await?);
 
-    // Cannot use .join(&str) - which finds the Boost
     member.set_transport_uri(&uri, "").await?;
 
     // eprintln!("{} <- {}", master.name().await?, member.name().await?);
@@ -63,21 +73,18 @@ async fn join(master: &Speaker, member: &Speaker) -> Result<(), sonor::Error> {
 }
 
 async fn do_list() -> Result<(), sonor::Error> {
-
     for (zone, speakers) in get_speakers().await?.into_iter() {
+        let uids: Vec<_> = futures::future::try_join_all(
+            speakers.iter().map(|s| s.uuid())
+        ).await?;
 
-        let mut uids = vec![];
-        for spkr in &speakers {
-            uids.push(spkr.uuid().await?);
-        }
-
-        println!("{}: {} - {:?}", zone, speakers.len(), uids);
+        println!("{}: {:?}", zone, uids);
     }
 
     Ok(())
 }
 
-/// Retun true for only those devices that are speakers.
+/// Return true for only those devices that are speakers.
 /// Which carry a service with service_id = "urn:upnp-org:serviceId:MusicServices"
 fn is_speaker(device: &Speaker) -> bool {
     device.device()
@@ -87,10 +94,10 @@ fn is_speaker(device: &Speaker) -> bool {
 }
 
 /// Return a list of speakers, that implement MUSIC_SERVICE.
-async fn get_speakers() -> Result<HashMap<String, Vec<Speaker>>, sonor::Error> {
-    let mut devices = sonor::discover(Duration::from_secs(5)).await?;   
+async fn get_speakers() -> Result<BTreeMap<String, Vec<Speaker>>, sonor::Error> {
+    let mut devices = sonor::discover(Duration::from_secs(5)).await?;
 
-    let mut result = HashMap::new();
+    let mut result = BTreeMap::new();
     while let Some(speaker) = devices.try_next().await? {
         if is_speaker(&speaker) {
             result.entry(speaker.name().await?)
